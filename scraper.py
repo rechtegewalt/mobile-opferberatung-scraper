282034152aa0c33d768702fabc458ebfd4dfcf37
Johannes Filter <hi@jfilter.de>
2018-11-05 21:15:21 +0100

    trying to get it into our format

--- scraper.py
+++ scraper.py
@@ -2,17 +2,19 @@ import datetime
 import glob
 import re
 import shutil
+from urllib.parse import quote
+import json
 
 import lxml.html
 import scraperwiki
 
 
-def process_one(entry):
+def process_one(entry, url):
     source_list = entry.xpath("./text()")
     if len(source_list) > 0:
         source = source_list[0]
     else:
-        source = "Unbekannt"
+        source = ""
 
     head = entry.xpath("./following-sibling::h1[1]/text()")[0]
 
@@ -30,6 +32,10 @@ def process_one(entry):
     # just location
     raw_location = " ".join(head_split[1 : len(head_split)])
 
+    county = re.match('"\(.*|\/.*"', raw_location)
+    if not county is None:
+        county = county.group(1)
+        county = county.replace("(", "").replace(")", "")
     # TODO: What is about landkreise?
     # but sometimes with 'Landkreisen'
     # we want to clean the data and remove them
@@ -38,10 +44,31 @@ def process_one(entry):
     # sometimes new white spaces need to be stripped again
     location = location.strip()
 
+    source_name = source
+    source_date = ""
+    if "," in source:
+        sep_source = source.rindex(",")
+        try:
+            source_date = datetime.datetime.strptime(
+                source[sep_source + 1 :], "%d.%m.%Y"
+            ).isoformat()
+            source_name = source[:sep_source]
+        except:
+            pass
+
+    uri = url + "#" + quote(date + "-" + location)
+
     scraperwiki.sqlite.save(
-        unique_keys=["source", "text", "date", "location"],
-        data={"source": source, "text": text, "date": date, "location": location},
-        table_name="data" # broken right now
+        unique_keys=["uri"],
+        data={
+            "sources": json.dumps({"name": source_name, "date": source_date}),
+            "description": text,
+            "startDate": date,
+            "locations": json.dumps([location, "Sachsen-Anhalt", "Germany"]),
+            "iso3166_2": "DE-ST",
+            "uri": uri,
+        },
+        table_name="data",  # broken right now
     )
 
 
@@ -61,7 +88,7 @@ for i in indices:
     doc = lxml.html.fromstring(html)
 
     for entry in doc.xpath("//h5"):
-        process_one(entry)
+        process_one(entry, url)
 
 # This is a hotfix. Right now, the custom naming of the table is broken.
-shutil.move(glob.glob('./*.sqlite')[0], "data.sqlite")
+shutil.move(glob.glob("./*.sqlite")[0], "data.sqlite")
