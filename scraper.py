ce4685112a9a4c4ec47aa9fce12263c5163b5764
Johannes Filter <hi@jfilter.de>
2019-02-23 14:36:34 +0100

    extract multiple sources and fix landkreise

--- scraper.py
+++ scraper.py
@@ -2,19 +2,91 @@ import datetime
 import glob
 import re
 import shutil
+import sys
 from urllib.parse import quote
-import json
 
 import lxml.html
 import scraperwiki
 
+# simple enviroment
+debug = len(sys.argv) > 1
+
+# 11.02.2005
+def extract_data_format_1(source, sep_source):
+    source_date_string = source[sep_source + 1 :].strip()
+    source_date = datetime.datetime.strptime(source_date_string, "%d.%m.%Y").isoformat()
+    source_name = source[:sep_source]
+    return source_name, source_date
+
+
+# 11.02.05
+def extract_data_format_2(source, sep_source):
+    source_date_string = source[sep_source + 1 :].strip()
+    source_date = datetime.datetime.strptime(source_date_string, "%d.%m.%y").isoformat()
+    source_name = source[:sep_source]
+    return source_name, source_date
+
+
+def extract_sources(source_list):
+    # sometimes they concat sources with ; so try to get them
+    source_list_fixed = []
+    for s in source_list:
+        source_list_fixed += s.split(";")
+    source_list = source_list_fixed
+
+    sources = []
+    for source in source_list:
+        source_name = source
+        source_date = ""
+        if "," in source:
+            sep_source = source.rindex(",")
+            done = False
+            # parse
+            try:
+                source_name, source_date = extract_data_format_1(source, sep_source)
+                done = True
+            except BaseException as e:
+                if debug:
+                    print(source)
+                    print(str(e))
+            if not done:
+                try:
+                    source_name, source_date = extract_data_format_2(source, sep_source)
+                except BaseException as e:
+                    if debug:
+                        print(source)
+                        print(str(e))
+        sources.append({"name": source_name, "date": source_date})
+
+    return sources
+
+
+def extract_location(location):
+    # print(raw_location)
+    county = re.findall(r"\(.*\)", location)
+    if len(county) > 0:
+        county = county[0]
+        county = county.replace("(", "").replace(")", "")
+
+        # special case for Halle Saale
+        if county == "Saale":
+            county = []
+        else:
+            # remove landkreis from location
+            location = re.sub(r"\(.*|\/.*", "", location)
+
+    # sometimes new white spaces need to be stripped again
+    location = location.strip()
+    if len(county) == 0:
+        location_final = ", ".join([location, "Sachsen-Anhalt", "Germany"])
+    else:
+        location_final = ", ".join([location, county, "Sachsen-Anhalt", "Germany"])
+    return location_final
+
 
 def process_one(entry, url):
     source_list = entry.xpath("./text()")
-    if len(source_list) > 0:
-        source = source_list[0]
-    else:
-        source = ""
+    sources = extract_sources(source_list)
 
     head = entry.xpath("./following-sibling::h1[1]/text()")[0]
 
@@ -25,56 +97,35 @@ def process_one(entry, url):
     text = re.sub(r"<!--.*-->", "", raw_text).strip()
 
     head_split = head.split()
-
     raw_date = head_split[0]
     date = datetime.datetime.strptime(raw_date, "%d.%m.%Y").isoformat()
-
     # just location
-    raw_location = " ".join(head_split[1 : len(head_split)])
-
-    county = re.match('"\(.*|\/.*"', raw_location)
-    if not county is None:
-        county = county.group(1)
-        county = county.replace("(", "").replace(")", "")
-    # TODO: What is about landkreise?
-    # but sometimes with 'Landkreisen'
-    # we want to clean the data and remove them
-    location = fix_location.sub("", raw_location)
-
-    # sometimes new white spaces need to be stripped again
-    location = location.strip()
-
-    source_name = source
-    source_date = ""
-    if "," in source:
-        sep_source = source.rindex(",")
-        try:
-            source_date = datetime.datetime.strptime(
-                source[sep_source + 1 :], "%d.%m.%Y"
-            ).isoformat()
-            source_name = source[:sep_source]
-        except:
-            pass
-
+    raw_location = " ".join(head_split[1:])
+    location = extract_location(raw_location)
     uri = url + "#" + quote(date + "-" + location)
 
     scraperwiki.sqlite.save(
         unique_keys=["uri"],
-        data={
-            "sources": json.dumps({"name": source_name, "date": source_date}),
-            "description": text,
-            "startDate": date,
-            "locations": json.dumps([location, "Sachsen-Anhalt", "Germany"]),
-            "iso3166_2": "DE-ST",
-            "uri": uri,
-        },
-        table_name="data",  # broken right now
+        data={"description": text, "startDate": date, "iso3166_2": "DE-ST", "uri": uri},
+        table_name="data",
     )
+
+    scraperwiki.sqlite.save(
+        unique_keys=["reportURI"],
+        data={"subdivisions": location, "reportURI": uri},
+        table_name="location",
+    )
+
+    for s in sources:
+        scraperwiki.sqlite.save(
+            unique_keys=["reportURI"],
+            data={"publishedDate": s["date"], "name": s["name"], "reportURI": uri},
+            table_name="source",
+        )
 
 
 base_url = "http://www.mobile-opferberatung.de/monitoring/chronik%s/"
 
-fix_location = re.compile("\(.*|\/.*")
 indices = range(2003, datetime.datetime.now().year + 1)
 
 for i in indices:
